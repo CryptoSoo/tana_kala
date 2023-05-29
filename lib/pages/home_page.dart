890a179035e059bcf8027d10c6cdec3fa6ea1eb6
Soo <cryptosoo@pm.me>
2023-05-29 19:56:12 +0300

    tana kala oomade

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -19,13 +19,14 @@ class _HomePageState extends State<HomePage> {
         backgroundColor: Colors.white38,
         searchTextKeyboardType: TextInputType.text,
         searchTextDirection: TextDirection.rtl,
-        iconTheme: IconThemeData(color: Colors.white),
-        titleTextStyle: TextStyle(),
-        title: Text('جستجو در تاناکالا', textDirection: TextDirection.rtl),
+        iconTheme: const IconThemeData(color: Colors.white),
+        titleTextStyle: const TextStyle(),
+        title:
+            const Text('جستجو در تاناکالا', textDirection: TextDirection.rtl),
         onSearch: (value) => setState(() => searchValue = value),
       ),
       body: Padding(
-          padding: EdgeInsets.all(20),
+          padding: const EdgeInsets.all(20),
           child: SingleChildScrollView(
             child: Column(
               children: [
@@ -34,34 +35,30 @@ class _HomePageState extends State<HomePage> {
                   child: Container(
                     width: double.infinity,
                     height: 210,
-                    color: Color.fromRGBO(55, 166, 216, 100),
+                    color: const Color.fromRGBO(55, 166, 216, 100),
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        Stack(
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.center,
                           children: [
-                            Row(
+                            Container(
+                                padding:
+                                    const EdgeInsets.symmetric(horizontal: 5),
+                                height: 159,
+                                width: 159,
+                                child: ClipRRect(
+                                  child: Image.asset('assets/image_15.png'),
+                                )),
+                            const Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
                               crossAxisAlignment: CrossAxisAlignment.center,
-                              mainAxisAlignment: MainAxisAlignment.center,
                               children: [
-                                Container(
-                                    padding:
-                                        EdgeInsets.symmetric(horizontal: 5),
-                                    height: 159,
-                                    width: 159,
-                                    child: ClipRRect(
-                                      child: Image.asset('assets/image_15.png'),
-                                    )),
-                                Column(
-                                  mainAxisAlignment: MainAxisAlignment.start,
-                                  crossAxisAlignment: CrossAxisAlignment.center,
-                                  children: [
-                                    Text('زمستون تو راهه...'),
-                                    Text(
-                                      'تمامی لباس های که برای زمستان نیاز دارید  را از تانا کالا بیابید',
-                                      textAlign: TextAlign.center,
-                                    )
-                                  ],
+                                Text('زمستون تو راهه...'),
+                                Text(
+                                  'تمامی لباس های که برای زمستان نیاز دارید  را از تانا کالا بیابید',
+                                  textAlign: TextAlign.center,
                                 )
                               ],
                             )
@@ -71,7 +68,10 @@ class _HomePageState extends State<HomePage> {
                     ),
                   ),
                 ),
-                Row(
+                SizedBox(
+                  height: 30,
+                ),
+                const Row(
                   mainAxisAlignment: MainAxisAlignment.end,
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
@@ -82,27 +82,86 @@ class _HomePageState extends State<HomePage> {
                   ],
                 ),
                 Container(
+                  color: const Color(0xF7FAFC),
                   height: 103,
                   width: double.infinity,
                   child: ListView(
+                    physics: const BouncingScrollPhysics(),
+                    reverse: true,
                     scrollDirection: Axis.horizontal,
                     children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Image.asset('assets/image_15.png'),
-                          Text('لباس زنانه')
-                        ],
+                      FavList(
+                        image: 'assets/image_1380.png',
+                        text: 'لباس زنانه',
                       ),
-                      Row(),
-                      Row(),
+                      FavList(
+                          text: 'لباس مردانه', image: 'assets/image_1381.png'),
+                      FavList(
+                          text: 'لباس بچه گانه',
+                          image: 'assets/image_1382.png'),
+                      FavList(
+                          text: 'لباس بچه گانه',
+                          image: 'assets/image_1383.png'),
+                      FavList(
+                          text: 'لباس بچه گانه',
+                          image: 'assets/image_1383.png'),
+                      FavList(
+                          text: 'لباس بچه گانه',
+                          image: 'assets/image_1383.png'),
                     ],
                   ),
-                )
+                ),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Icon(Icons.arrow_back_sharp),
+                    Text('مشاهده همه'),
+                    Spacer(),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(
+                          'پیراهن مردانه',
+                          style: TextStyle(fontSize: 24),
+                        ),
+                        Text(
+                          'بر اساس بازدید های اخیر شما',
+                          style: TextStyle(fontSize: 12),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
               ],
             ),
           )),
+    );
+  }
+}
+
+class FavList extends StatelessWidget {
+  String text;
+  String image;
+
+  FavList({
+    required this.text,
+    required this.image,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Text(
+          text,
+        ),
+        Image.asset(image),
+      ],
     );
   }
 }
